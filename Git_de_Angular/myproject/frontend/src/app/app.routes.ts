3528963e256a0be1eee3f391fe11daa8c6cf24d1
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-03 13:05:14 -0500

    Se configuran las rutas en angular

--- Git_de_Angular/myproject/frontend/src/app/app.routes.ts
+++ Git_de_Angular/myproject/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VideosComponent } from './videos/videos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'videos', component: VideosComponent }
+];
