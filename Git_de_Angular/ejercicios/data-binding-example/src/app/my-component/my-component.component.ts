a740f009cab7d1b1e76a4ca6b5eb7a617a8e5aed
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-08 12:02:07 -0500

    Se configura el typescript de my component

---
+++ Git_de_Angular/ejercicios/data-binding-example/src/app/my-component/my-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponent {
+  title: string = 'Ejemplo de data Blinding de clases teorica!';
+  
+  userInput: string = '';
+}
