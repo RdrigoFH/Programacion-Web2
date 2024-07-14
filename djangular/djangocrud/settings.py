54c8583dffa0b911654118817707e83d02109624
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-13 20:06:46 -0500

    se habilita el manejo de solicitudes cors y se gestiona el manejo de solicitude http, todo se configura en settings.py

--- djangular/djangocrud/settings.py
+++ djangular/djangocrud/settings.py
@@ -53,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
@@ -127,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200"
+]
+
